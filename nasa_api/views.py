6c31acd265e7ed3608f1a61810664f297eb5c200
Bartosz Wojcik <bartzhome@hotmail.com>
2021-11-21 16:16:05 -0500

    added comments

--- nasa_api/views.py
+++ nasa_api/views.py
@@ -1,11 +1,28 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+"""
+Django web app that communicates with NASA API - APOD. User can select range of dates to search pictures of the day.
+App will return from date range
+
+TODO:
+    - Advanced search
+        - Thumbnails
+        - Number of results to return
+    - Utlities
+        - Sorting
+"""
+
+from django.shortcuts import render
 from datetime import datetime
 import requests
-import json
 
 
-def index(request):
+def home(request):
+    """
+    Home function handles user interaction with web application.
+    If not a post request - display initial page with a todays date as default for end date
+    If post request with start and end dates - validate dates, return search results or error message
+    :param request: request from client side
+    :return: search results or error message
+    """
 
     # if not a post request, display page
     if request.method != "POST":
@@ -22,7 +39,7 @@ def index(request):
             date_now = datetime.today()
         except ValueError:
             message = "Incorrect data format, should be YYYY-MM-DD"
-            return render(request, 'base.html', {'message': message})
+            return render(request, 'base.html', {'message': message, 'date': date_now})
 
         # validate that start date is less than end date and end_data is equal or less than todays date
         if start_date < end_date <= date_now:
@@ -35,6 +52,11 @@ def index(request):
             date_now = datetime.today().strftime('%Y-%m-%d')
             return render(request, 'home.html', {'results': results, 'date': date_now})
 
+        else:
+            # return with error message
+            message = "Start Date must be before End Date and End Date cannot be date in future."
+            return render(request, 'base.html', {'message': message, 'date': date_now})
+
     else:
         message = "Invalid Date Entry. Please try again."
         date_now = datetime.today().strftime('%Y-%m-%d')
@@ -42,6 +64,14 @@ def index(request):
 
 
 def search_results(request,start_date, end_date):
+    """
+    Function makes a call to NASA APOD API to retreive data from user selected date range.
+    :param request: user request data
+    :param start_date: a valid start date
+    :param end_date: a valid end date
+    :return: return json data for date range from nasa api response or connection error message
+    """
+
     # api key
     API_KEY = "EAnJsZsdRGv4hM0zeLUKosCya5AkJV1GP5jDHRxQ"
 
@@ -53,7 +83,8 @@ def search_results(request,start_date, end_date):
               'end_date': end_date,
               'api_key': API_KEY}
 
-    # try for a get request from url with parameters
+    # try for a get request response from url with parameters
+    # if not connection can be made, except and send connection error message
     try:
         response = requests.get(url=url, params=params)
         json_data = response.json()
